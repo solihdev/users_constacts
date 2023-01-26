dfa25227cccc96021a1ae63ba25f196d44bd43cc
Abdumutalibovo <odilbekjuraev07@gmail.com>
2023-01-26 17:49:22 +0500

    some changes

--- lib/ui/tab_box/tabs/get_contact/get_contacts.dart
+++ lib/ui/tab_box/tabs/get_contact/get_contacts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_contacts_with_bloc/bloc/contacts_bloc/contacts_bloc.dart';
 import 'package:user_contacts_with_bloc/bloc/get_contacts/get_contacts_cubit.dart';
 import 'package:user_contacts_with_bloc/bloc/get_contacts/get_contacts_state.dart';
 
@@ -12,23 +13,40 @@ class GetContactsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Get Contacts"),
       ),
-      body: BlocBuilder<GetContactCubit, GetContactsState>(
+      body: BlocConsumer<GetContactCubit, GetContactsState>(
         builder: (context, state) {
-          if(state is GetContactsLoading){
-            return const Center(child: CircularProgressIndicator(),);
+          if (state is GetContactsLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           }
-          if(state is GetContactsSuccess){
+          if (state is GetContactsSuccess) {
             return ListView(
               children: List.generate(state.contactModel.length, (index) {
-                var item= state.contactModel[index];
+                var item = state.contactModel[index];
                 return ListTile(
                   title: Text(item.name),
                   subtitle: Text(item.number),
+                  leading: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      BlocProvider.of<ContactsBloc>(context).add(
+                        DeleteContact(
+                          contactId: int.parse(
+                            item.id.toString(),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 );
               }),
             );
           }
           return const SizedBox();
+        },
+        listener: (context,state){
+          // BlocProvider.of<GetContactCubit>(context).fetchContacts();
         },
       ),
     );
