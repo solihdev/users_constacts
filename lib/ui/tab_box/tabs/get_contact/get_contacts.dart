3942f0a02b13ae96a9ac1ebbe8a1fdb040665f3a
Abdumutalibovo <odilbekjuraev07@gmail.com>
2023-01-26 20:38:13 +0500

    add url launcher

--- lib/ui/tab_box/tabs/get_contact/get_contacts.dart
+++ lib/ui/tab_box/tabs/get_contact/get_contacts.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:user_contacts_with_bloc/cubit/get_contacts/get_contacts_cubit.dart';
 import 'package:user_contacts_with_bloc/cubit/get_contacts/get_contacts_state.dart';
 import 'package:user_contacts_with_bloc/ui/tab_box/tabs/widgets/update_widget.dart';
@@ -25,15 +26,21 @@ class GetContactsScreen extends StatelessWidget {
               children: List.generate(state.contactModel.length, (index) {
                 var item = state.contactModel[index];
                 return ListTile(
+                  onLongPress: () {
+                    launchUrl(
+                      Uri.parse("tel://${item.number}"),
+                    );
+                  },
+                  onTap: (){
+                    showDialog(
+                      context: context,
+                      builder: (context) =>
+                          UpdateContactDialog(contactModel: item),
+                    );
+                  },
                   title: Text(item.name),
                   subtitle: Text(item.number),
-                  trailing:  IconButton(
-                    icon: const Icon(Icons.edit),
-                    onPressed: (){
-                      showDialog(context: context, builder: (context) => UpdateContactDialog(contactModel: item),);
-                    },
-                  ),
-                  leading: IconButton(
+                  trailing: IconButton(
                     icon: const Icon(Icons.delete),
                     onPressed: () {
                       context.read<GetContactCubit>().deleteContact(item.id!);
